af0e8b42b6d5aeddcfc1850a3ebf051dbb4c89b2
Camille MESHORER <camille.meshorer@opendigitaleducation.com>
2023-11-13 16:05:51 +0100

    chore: add auth store

--- front/src/store.ts
+++ front/src/store.ts
@@ -1,5 +1,9 @@
-import {configureStore} from '@reduxjs/toolkit';
+import create from 'zustand';
 
-export default configureStore({
-  reducer: {},
-});
+export const useAuthStore = create(set => ({
+  token: null,
+  // @ts-ignore
+  // Issue typing the set() function via suggested implementation
+  // source: https://docs.pmnd.rs/zustand/guides/typescript
+  storeToken: (token: string | null) => set({token}),
+}));
